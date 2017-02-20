28481cdae845f64490abfef2b1f37df5a1c980e9
MarinoVP <marinovp21@gmail.com>
2017-02-20 00:12:51 -0500

    Week5_RepetitionWhile

---
+++ COP1000/Week5_RepetitionWhile.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+
+	/*
+	
+			COUNTER CONTROL LOOP
+			
+	*/
+
+
+	/*
+	//While Loop 1 - 10
+	int count = 1;
+
+	while (count <= 10) {
+		printf("%i \n", count);
+		count = count + 1;
+		}
+		
+		*/
+
+	// While Loop multiples of 5 less than 100
+/*
+	int count = 5;
+
+	while (count <= 100) {
+		printf("%i \n", count);
+		count = count + 5;
+	}
+		
+	*/
+	
+	//While Loop even numbers in desc order from 70 - 50 inclusive
+	/*
+	int count = 70;
+
+
+	while (count >= 50) {
+		printf("%i \n", count);
+		count = count - 2;
+	}
+	*/
+
+	/*
+	int count = 1, sum=0;
+
+
+	while (count <= 100) {
+		
+		sum = sum + count;
+		count = count + 1;
+	}
+
+	printf("%i \n", sum);
+
+
+	*/
+
+	//   SENTINEL CONTROLLED LOOP
+
+	/*
+	Ask user to enter test scores. When they have entered all the scores, they will enter the value -1.
+	The program will sum up all the scored entered, and output sum
+	*/
+
+
+	int score, sum = 0;
+	printf("Enter a test score (-1 to quit): \n");
+	scanf_s("%i", &score);
+	while (score != -1) {
+		sum = sum + score;
+		printf("Enter a test score (-1 to quit): \n");
+			scanf_s("%i", &score);
+		}
+
+	printf("The sum of the score  is %i \n", sum);
+
+	system("pause");
+}
